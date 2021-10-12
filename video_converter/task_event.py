91ed26aafad256b66b1b75dcf5a61118bc6df090
SolardiaX <solardiax@hotmail.com>
2021-10-12 09:43:37 +0800

    feat: enhance MediaConvert event handler

--- video_converter/task_event.py
+++ video_converter/task_event.py
@@ -11,25 +11,34 @@ logger = logging.getLogger(__name__)
 def lambda_handler(event, _):
     logger.info("Received event: " + json.dumps(event, indent=2))
 
+    code = 200
+    error = None
+    progress = None
+
     itemid = event['detail']['jobId']
     status = event['detail']['status']
 
     taskitem = get_task_item(itemid)
     if taskitem is not None:
-        update_taskitem_status(itemid, status)
-
         if status == 'COMPLETE':
             increase_task_finished_counter(taskitem.taskid)
         elif status == 'ERROR':
+            error = event['detail']['errorMessage']
             increase_task_error_counter(taskitem.itemid)
         elif status == 'STATUS_UPDATE':
-            progress = event['detail']['jobProgress']['jobPercentComplete']
-            update_taskitem_process(itemid, math.floor(float(progress)))
+            progress = math.floor(float(event['detail']['jobProgress']['jobPercentComplete']))
+            update_taskitem_progress(itemid, progress)
 
-        logger.info("Job(%s) status is updated to [%s] " % (itemid, status))
-        return {"status": 200, "event": event, 'message': None}
+        if status != 'STATUS_UPDATE':
+            update_taskitem_status(itemid, status, error)
+            update_taskitem_progress(itemid, 100 if status == 'COMPLETE' else '-1')
+
+        logger.info("Job(%s) status is updated to [%s] "
+                    % (itemid, str(progress) if status == 'STATUS_UPDATE' else status))
     else:
+        code = 400
+        error = "Job not exists"
+
         logger.info("Job(%s) not exists. " % itemid)
 
-    logger.info("Job(%s) status is unknown - [%s] " % (itemid, status))
-    return {"status": 400, "event": event, 'message': 'Unknown status.'}
+    return {"status": code, "event": event, 'message': error}
